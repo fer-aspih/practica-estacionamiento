4a793cae7799d4afa92bda1a2259ec6bf9fc02ab
fer-aspih <fercho030786@gmail.com>
2025-01-22 21:51:14 -0600

    Se agregaron validaciones a los campos

--- src/EjercicioIntegradorRepetitivas.java
+++ src/EjercicioIntegradorRepetitivas.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EjercicioIntegradorRepetitivas {
 
@@ -29,20 +31,46 @@ public class EjercicioIntegradorRepetitivas {
 
         while (!placa.equalsIgnoreCase("fin")) {
             try {
-            System.out.println("Ingrese la placa de su vehiculo");
-            placa = sc.nextLine();
-
-            if (!placa.equalsIgnoreCase("fin"))
-                System.out.println("Ingrese el tipo de Servicio");
-                System.out.println("1 - Por hora");
-                System.out.println("2 - Media Jornada (5 horas)");
-                System.out.println("3 - Jornada completa (Hasta 10 horas");
-                sc = new Scanner(System.in);
-                tipoServicio = sc.nextInt();
-
-                if (tipoServicio <= 0 || tipoServicio > 3) {
-                    System.out.println("No ingreso un tipo de servicio correcto");
+                boolean placaValida = false;
+                while (!placaValida) {
+                System.out.println("Ingrese la placa de su vehiculo(maximo 6 caracteres o escriba 'fin' para terminar");
+                placa = sc.nextLine();
+                if (placa.length() > 6) {
+                    System.out.println("Error: La placa no puede tener más de 6 caracteres.");
+                } else if (!placa.equalsIgnoreCase("fin")) {
+                    if (esPlacaValida(placa)) {
+                        System.out.println(placa + " es válida");
+                        placaValida = true;
+                    } else {
+                        System.out.println(placa + " no es válida");
+                    }
                 } else {
+                    placaValida = true;// Para salida del bucle si se ingresa "fin"
+                  }
+                }
+                if (!placa.equalsIgnoreCase("fin")) {
+                    boolean inputValid = false;
+
+                    while (!inputValid) {
+                        System.out.println("Ingrese el tipo de Servicio");
+                        System.out.println("1 - Por hora");
+                        System.out.println("2 - Media Jornada (5 horas)");
+                        System.out.println("3 - Jornada completa (Hasta 10 horas");
+
+                        if (sc.hasNextInt()) {
+                            tipoServicio = sc.nextInt();
+                            sc.nextLine(); // Limpiar buffer
+
+                            if (tipoServicio > 0 && tipoServicio <= 3) {
+                                inputValid = true;
+                            } else {
+                                System.out.println("No ingreso un tipo de servicio correcto");
+                            }
+                        } else {
+                            System.out.println("Entrada  no valida debe ingresar un numero.");
+                            sc.nextLine(); // Limpiar buffer
+                        }
+                    }
                 /* Los tpos de estacionamineto disponibles son 3:
                     -Por hora (cuyo valor es de $3 USD por hora
                     -Media jornada (cuyo valor es de $15 USD y posee ub 5% de descuento)
@@ -71,12 +99,14 @@ public class EjercicioIntegradorRepetitivas {
                         cont3 = cont3 + 1;
                         totalDia = totalDia + total;
                     }
+                    System.out.println("*************MUCHAS GTRACIAS POR SU COMPRA!*************\n");
                 }
-                System.out.println("*************MUCHAS GTRACIAS POR SU COMPRA!*************\n");
 
-        } catch (NumberFormatException e) {
-            System.out.println("Error de entrada: Debe ingresar un número para el tipo de servicio y cantidad de horas.");
-        }
+                sc = new Scanner(System.in);
+
+            } catch (NumberFormatException e) {
+                System.out.println("Error de entrada: Debe ingresar un número para el tipo de servicio y cantidad de horas.");
+            }
         }
 
         System.out.println("===============================================================");
@@ -85,5 +115,15 @@ public class EjercicioIntegradorRepetitivas {
         System.out.println("Cantidad servicios por Media jornada: " + cont2);
         System.out.println("Cantidad servicios por Jornada completa: " + cont3);
         System.out.println("El monto total recaudado por dia es de: $" + totalDia + " USD ");
+
+        sc.close();
+    }
+
+    public static boolean esPlacaValida(String placa) {
+        // Expresión regular que coincide con 6 letras o números
+        String patron = "^[A-Za-z0-9]{6}$"; // Esta expresión regular valida que la cadena tenga exactamente seis caracteres, todos siendo letras o números.
+        Pattern pattern = Pattern.compile(patron); // Compilamos la expresión regular definida en patron en un objeto Pattern
+        Matcher matcher = pattern.matcher(placa); // Es una clase que se utiliza para realizar operaciones de coincidencia en una cadena, para verificar si coincide con la expresión regular.
+        return matcher.matches(); // Esta línea utiliza el objeto matcher para verificar si toda la cadena placa coincide con la expresión regular
     }
 }
